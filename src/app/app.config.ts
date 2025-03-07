d2457297a00c1d2b511b1ff71cdad331f129171a
Leonel Rosales <leonel.rosales@softtek.com>
2025-03-07 23:33:50 +0100

    remove zone conflicts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,8 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { BrowserModule } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [ importProvidersFrom(BrowserModule), provideRouter(routes)]
 };
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { createCustomElement } from '@angular/elements';
-import { ApplicationRef } from '@angular/core';
+import { ApplicationRef, inject } from '@angular/core';
 import { MyWidgetComponent } from './app/my-widget/my-widget.component';
 import { appConfig } from './app/app.config';
 
+// 🔥 REMOVE Zone.js to prevent conflicts
+(window as any).Zone = undefined;
+
 bootstrapApplication(MyWidgetComponent, appConfig).then((applicationRef: ApplicationRef) => {
   const webComponent = createCustomElement(MyWidgetComponent, { injector: applicationRef.injector });
-  customElements.define('my-angular-widget', webComponent);
-}).catch((err) => console.error(err));
-
+  if (!customElements.get('my-angular-widget')) {
+    customElements.define('my-angular-widget', webComponent);
+  }
+    // ✅ Manually trigger Angular’s change detection if needed
+    const appRefInstance = inject(ApplicationRef);
+    setInterval(() => appRefInstance.tick(), 1000);
+}).catch((err) => console.error('Angular Web Component Error:',err));
 
