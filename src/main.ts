900b3967d31f26bcd70fa2540f1881fde855cde9
Leonel Rosales <leonel.rosales@softtek.com>
2025-03-07 23:17:59 +0100

    fix config

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { createCustomElement } from '@angular/elements';
-import { importProvidersFrom } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { ApplicationRef } from '@angular/core';
 import { MyWidgetComponent } from './app/my-widget/my-widget.component';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(MyWidgetComponent, {
-  providers: [importProvidersFrom(BrowserModule)]
-}).then(appRef => {
-  const injector = appRef.injector;
-  const webComponent = createCustomElement(MyWidgetComponent, { injector });
+bootstrapApplication(MyWidgetComponent, appConfig).then((applicationRef: ApplicationRef) => {
+  const webComponent = createCustomElement(MyWidgetComponent, { injector: applicationRef.injector });
   customElements.define('my-angular-widget', webComponent);
-});
+}).catch((err) => console.error(err));
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 const path = require('path');
-
-module.exports = {
-  entry: './dist/commercetool/browser/main.js',
-  output: {
-    filename: 'custom-angular-widget.js',
-    path: path.resolve(__dirname, 'dist/commercetool'),
-  },
-  mode: 'production'
+module.exports = () => {
+  return {
+    entry: [
+      './dist/commercetool/browser/main.js', 
+    ],
+    output: {
+      filename: 'custom-angular-widget.js',
+      path: path.resolve(__dirname, 'dist/commercetool'),
+    },
+    mode: 'production'
+  }
 };
