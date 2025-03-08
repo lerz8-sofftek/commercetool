6ed303ddda426baa3ced475ece738207a0308b72
Leonel Rosales <leonel.rosales@softtek.com>
2025-03-08 01:45:35 +0100

    my-component part 2

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { createCustomElement } from '@angular/elements';
 import { ApplicationRef, enableProdMode } from '@angular/core';
-import { MyWidgetComponent } from './app/my-widget/my-widget.component';
 import { appConfig } from './app/app.config';
+import { SimpleButtonComponent } from './app/my-widget/my-widget.component';
 
 enableProdMode(); // Ensure production mode is enabled
-bootstrapApplication(MyWidgetComponent, appConfig).then((applicationRef: ApplicationRef) => {
-  const webComponent = createCustomElement(MyWidgetComponent, { injector: applicationRef.injector });
+bootstrapApplication(SimpleButtonComponent, appConfig).then((applicationRef: ApplicationRef) => {
+  const webComponent = createCustomElement(SimpleButtonComponent, { injector: applicationRef.injector });
   if (!customElements.get('my-angular-widget')) {
     customElements.define('custom-element-angular-widget', webComponent);
   }
--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,11 @@ module.exports = () => {
   return {
     entry: [
       './dist/commercetool/browser/main.js', 
+      './dist/commercetool/browser/polyfills.js', 
     ],
     output: {
-      filename: 'custom-angular-widget.js',
+      filename: 'custom-element-angular-widget.js',
       path: path.resolve(__dirname, 'dist/commercetool'),
-    },
-    externals: {
-      'zone.js': 'Zone' // 🚀 Exclude zone.js from final bundle
     },
     mode: 'production'
   }
